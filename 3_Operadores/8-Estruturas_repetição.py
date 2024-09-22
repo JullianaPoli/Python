be226efa02de8c4ae08eba264cda4d02c8eef4cb
JUliana Poli <juh_juli.ana@hotmail.com>
2024-09-22 17:20:11 -0300

    Atualização de estudo Python

---
+++ 3_Operadores/8-Estruturas_repetição.py
@@ -0,0 +1,19 @@
+ # Identação e bloco
+ # Utilizando espaços ( a cada novo codigo, utilizamos 4 espaços em branco.)
+ 
+ # EXEMPLO
+ 
+def sacar (valor):
+    saldo = 500
+    
+    if saldo >= valor:
+         print("valor sacado")
+         print("retire o seu dinheiro na boca do caixa")
+  
+         
+    print("Obrigada por ser nosso cliente, tenha um bom dia!")     
+         
+sacar(100) 
+
+
+
